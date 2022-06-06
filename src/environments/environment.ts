ea3a8c4964682d92351946fcb8309fda1856b059
Binh Do <binh@edutalkapp.com>
2022-06-06 18:04:09 +0700

    update assets for 2022

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://thanhnienapi.edutalk.edu.vn',
+    issuer: 'https://centechltd.com',
     redirectUri: baseUrl,
     clientId: 'ThanhNien_App',
     responseType: 'code',
@@ -18,7 +18,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'https://thanhnienapi.edutalk.edu.vn',
+      url: 'https://centechltd.com',
       rootNamespace: 'ThanhNien',
     },
   },
